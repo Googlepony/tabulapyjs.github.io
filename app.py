170a3ff8bf915a24b03653f091e60259ae11aaaa
Googlepony <78266591+Googlepony@users.noreply.github.com>
2023-02-24 18:22:18 +0530

    Version 1

--- app.py
+++ app.py
@@ -1,14 +1,15 @@
 import csv
 import tabula
 from flask import Flask, request, jsonify
-import os
+
+app = Flask(__name__)
 
 
 def convert_pdfs_to_csv(pdfs):
     csv_data = []
     for pdf in pdfs:
-        tabula.convert_into(pdf, 'converted.csv',
-                            output_format="csv", pages="all", area=(80, 30, 1080, 810))
+        tabula.convert_into(pdf, 'converted.csv', output_format="csv",
+                            pages="all", area=(80, 30, 1080, 810))
         with open('converted.csv', 'r') as file:
             reader = csv.reader(file)
             csvvalues = list(reader)
@@ -16,7 +17,9 @@ def convert_pdfs_to_csv(pdfs):
     return csv_data
 
 
-app = Flask(__name__)
+@app.route('/', methods=['GET'])
+def index():
+    return 'Get - Working!'
 
 
 @app.route('/convert', methods=['POST'])
@@ -28,33 +31,3 @@ def convert():
 
 if __name__ == '__main__':
     app.run(debug=True)
-
-
-# import csv
-# import tabula
-# tabula.convert_into('data.pdf', 'converted.csv',
-#                     output_format="csv", pages="all", area=(80, 30, 1080, 810))
-# with open('converted.csv', 'r') as file:
-#     reader = csv.reader(file)
-#     csvvalues = list(reader)
-
-# import os
-# import csv
-# import tabula
-
-# files = os.listdir()
-# for file in files:
-#     if file.endswith(".pdf"):
-#         tabula.convert_into(file, 'converted.csv', output_format="csv",
-#                             pages="all", area=(80, 30, 1080, 810))
-
-#         csvvalues = []
-#         for file in files:
-#             if file.endswith(".csv"):
-#                 with open(file, 'r') as file:
-#                     reader = csv.reader(file)
-#                     csvvalues.extend(list(reader))
-
-#                     with open('merged.csv', 'w', newline='') as file:
-#                         writer = csv.writer(file)
-#                         writer.writerows(csvvalues)
